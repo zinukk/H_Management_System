ec2e8ef0e0b4d89a0fb80fa037d220c9799a99aa
zinukk <slsxpseh54@naver.com>
2023-01-21 20:12:16 +0900

    ADD : initial setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from 'react';
+import type { AppProps } from 'next/app';
+import { Global, ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
+import global from 'styles/global';
+import theme from 'styles/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <Global styles={global} />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
